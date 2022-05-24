af4369f6af9df2175041ab45aa1cb3f142919fe0
DiogoCarvalhoo <dfac@ua.pt>
2022-05-24 12:18:05 +0100

    deployment fix

--- web-app/scripts/app_start.sh
+++ web-app/scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/web-app/src
 npm start > app.out.log 2> app.err.log < /dev/null &
-pm2 start npm --name "covidapp" -- start
+pm2 start npm --name "app" -- start
 pm2 startup
 pm2 save
 pm2 restart all
