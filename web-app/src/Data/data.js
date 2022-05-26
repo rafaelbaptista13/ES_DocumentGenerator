c35ef4c6fc1b862f9b7892510b20998580f96e09
DiogoCarvalhoo <dfac@ua.pt>
2022-05-26 12:25:46 +0100

    final deploy i hope

--- web-app/src/Data/data.js
+++ web-app/src/Data/data.js
@@ -1,6 +1,6 @@
-const urlAPI = process.env.NODE_ENV === "development" ? 'http://localhost:4000/' : 'http://ec2-107-21-161-171.compute-1.amazonaws.com:4000';
-const urlWeb = process.env.NODE_ENV === "development" ? 'http://localhost:3000/' : 'http://ec2-107-21-161-171.compute-1.amazonaws.com:3000'
-//const urlAPI = 'http://ec2-107-21-161-171.compute-1.amazonaws.com:4000'
-//const urlWeb = 'http://ec2-107-21-161-171.compute-1.amazonaws.com:3000'
+//const urlAPI = process.env.NODE_ENV === "development" ? 'http://localhost:4000/' : 'http://ec2-107-21-161-171.compute-1.amazonaws.com:4000';
+//const urlWeb = process.env.NODE_ENV === "development" ? 'http://localhost:3000/' : 'http://ec2-107-21-161-171.compute-1.amazonaws.com:3000'
+const urlAPI = 'http://ec2-107-21-161-171.compute-1.amazonaws.com:4000'
+const urlWeb = 'http://ec2-107-21-161-171.compute-1.amazonaws.com:3000'
 
 export {urlAPI, urlWeb};
