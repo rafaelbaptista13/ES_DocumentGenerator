7e5aa9fa51312a2e571c4eb6da1191f97c342c21
dgcunhaua <dgcunha@ua.pt>
2022-05-31 15:50:54 +0100

    Enpoint receives json and template and generates the repective output

--- server/app/controllers/document.controller.js
+++ server/app/controllers/document.controller.js
@@ -1,74 +1,91 @@
-const { Console } = require("console");
-const { StringDecoder } = require("string_decoder");
+const Excel = require("exceljs");
 
 // Generate a document
 exports.generate = (req, res) => {
-	console.log(req.file);
+	let template = req.files.template[0];
+	let jsonfile = req.files.jsonfile[0];
 
 	/* Validate request section */
-	if (req.file == undefined) {
+	if (template == undefined) {
 		res.status(400).send({
-			message: "You must send a JSON file in order to generate a file.",
+			message:
+				"You must send a 'xlsx', 'pptx' or 'docx' file in order to generate a file.",
 		});
 		return;
 	}
 
-	var filename = req.file.originalname;
-	var fileformat = filename.split(".").pop();
-
-	if (fileformat != "json") {
+	if (jsonfile == undefined) {
 		res.status(400).send({
-			message:
-				"Document format is not supported. Supported formats: [JSON].",
+			message: "You must send a JSON file in order to generate a file.",
 		});
 		return;
 	}
 	/* End validate request section */
 
 	const fs = require("fs");
-	let rawdata = fs.readFileSync(req.file.path, "utf-8");
+	let rawdata = fs.readFileSync(jsonfile.path, "utf-8");
 	let json = JSON.parse(rawdata);
 
-	readJSON(json);
+	let success = false;
+	if (
+		template.mimetype ===
+		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	)
+		success = readJSON_writeExcel(template, json);
 
+	// Return message
+	if (success) {
+		res.status(200).send({
+			message: "Document generated with sucess!",
+		});
+	} else {
+		res.status(500).send({
+			message: "Error Generating the Document",
+		});
+	}
 
-	// async doesnt finish????
-	fs.unlink(req.file.path, (err) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
+	// Delete temporary files
+	fs.unlinkSync(jsonfile.path);
+	fs.unlinkSync(template.path);
 
-		//file removed
-	});
-
-	res.status(200).send({
-		message: "Document generated with sucess!",
-	});
+	return;
 };
 
-function readJSON(json) {
+function readJSON_writeExcel(template, json) {
+	var workbook = new Excel.Workbook();
 
+	workbook.xlsx
+		.readFile(template.path)
+		.then(function () {
+			let pages = Object.keys(json);
+			var firstLetterIdx = "A".charCodeAt() - 1;
 
-	let pages = Object.keys(json);
+			pages.forEach((element) => {
+				const pageJson = json[element];
+				const pagename = Object.keys(pageJson);
+				var worksheet = workbook.getWorksheet(String(pagename));
 
-	pages.forEach(element => {
-		const pageJson = json[element];
-		console.log("PAGE: " + element);
-		
-		const pagename = Object.keys(pageJson);
-		pagename.forEach(element => {
-			const newJson = pageJson[element];
+				pagename.forEach((element) => {
+					const newJson = pageJson[element];
 
-			console.log("PAGE NAME: " + pagename);
-			console.log("Fields: ");
-
-			const fields = Object.keys(newJson);
-			fields.forEach(element => {
-				console.log(newJson[element]);
+					const fields = Object.keys(newJson);
+					fields.forEach((element) => {
+						var key = Object.keys(newJson[element]);
+						var col_row = String(key).match(/[a-zA-Z]+|[0-9]+/g);
+						var col_number =
+							col_row[0].charCodeAt() - firstLetterIdx;
+						worksheet.getRow(col_row[1]).getCell(col_number).value =
+							newJson[element][key];
+					});
+				});
 			});
+
+			workbook.xlsx.writeFile("Excel.xlsx");
+		})
+		.catch((err) => {
+			console.error(err.message);
+			return false;
 		});
 
-	});
- } 
- 
+	return true;
+}
--- server/app/routes/document.routes.js
+++ server/app/routes/document.routes.js
@@ -1,25 +1,45 @@
-const multer = require('multer');
+const multer = require("multer");
+
+const documentsFilter = (req, file, cb) => {
+	if (
+		file.mimetype == "application/json" ||
+		file.mimetype ==
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
 
 const upload = multer({
-  dest: "uploads/" // "uploads"
+	dest: "uploads/", // "uploads"
+	fileFilter: documentsFilter,
 });
 
-module.exports = app => {
-    const document = require("../controllers/document.controller.js");
-    var router = require("express").Router();
+module.exports = (app) => {
+	const document = require("../controllers/document.controller.js");
+	var router = require("express").Router();
 
-    /**
-     * @swagger
-     * /api/generate/:
-     *  post:
-     *    description: Generate a document from a template and a JSON file
-     *    responses: 
-     *      '200':
-     *         description: Document generation was performed with success
-     *      '500':
-     *         description: An internal error has occoured
-     */
-    router.post("/", upload.single('file'), document.generate);
-  
-    app.use('/api/generate', router);
-  };
+	/**
+	 * @swagger
+	 * /api/generate/:
+	 *  post:
+	 *    description: Generate a document from a template and a JSON file
+	 *    responses:
+	 *      '200':
+	 *         description: Document generation was performed with success
+	 *      '500':
+	 *         description: An internal error has occoured
+	 */
+	router.post(
+		"/",
+		upload.fields([
+			{ name: "template", maxCount: 1 },
+			{ name: "jsonfile", maxCount: 1 },
+		]),
+		document.generate
+	);
+
+	app.use("/api/generate", router);
+};
