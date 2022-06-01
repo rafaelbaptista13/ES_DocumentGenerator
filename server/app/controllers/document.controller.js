2f9965cd19d744488e44fd9aebf2c59ba48718c6
dgcunhaua <dgcunha@ua.pt>
2022-06-02 00:31:19 +0100

    storing generated template in s3

--- server/app/controllers/document.controller.js
+++ server/app/controllers/document.controller.js
@@ -6,13 +6,12 @@ const fs = require("fs");
 exports.generate = async (req, res) => {
 	let jsonfile = req.files.jsonfile[0];
 	let template_name = req.body.template_name;
-	let template_path = "uploads/"+template_name;
+	let template_path = "uploads/" + template_name;
 
 	/* Validate request section */
 	if (template_name == undefined) {
 		res.status(400).send({
-			message:
-				"You must send the template file name.",
+			message: "You must send the template file name.",
 		});
 		return;
 	}
@@ -29,13 +28,16 @@ exports.generate = async (req, res) => {
 		Bucket: process.env.AWS_BUCKET_NAME,
 		Key: "templates/" + req.body.template_name,
 	};
-	const template = await s3.getObject(params).promise().catch((err) => {
-		console.error(err.message);
-		return false;
-	});
+	const template = await s3
+		.getObject(params)
+		.promise()
+		.catch((err) => {
+			console.error(err.message);
+			return false;
+		});
 
 	// Case of bucket error
-	if(template === false) {
+	if (template === false) {
 		res.status(500).send({
 			message: "Template was not found.",
 		});
@@ -44,20 +46,27 @@ exports.generate = async (req, res) => {
 
 	// Temporary store the file
 	fs.writeFileSync(template_path, template.Body);
-
 	// Read json file and parse it
 	let rawdata = fs.readFileSync(jsonfile.path, "utf-8");
 	let json = JSON.parse(rawdata);
 
-	let success = false;
+	// TODO: CHECK FILE TYPE
+	let extension = template_path.split(".").pop();
 
-	// TODO: CHECK FILE TYPE;
-	success = await readJSON_writeExcel(template_path, json);	// Open and rewrite the excel file
+	let downloadURL;
+	if (extension === "xlsx")
+		downloadURL = await readJSON_writeExcel(
+			template_name,
+			template_path,
+			json
+		); // Open and rewrite the excel file
 
-	// Return message
-	if (success) {
+	// Return message		TODO: Send link with generated file
+	if (downloadURL) {
+		fs.unlinkSync("Excel.xlsx");
 		res.status(200).send({
 			message: "Document generated with sucess!",
+			downloadURL: downloadURL,
 		});
 	} else {
 		res.status(500).send({
@@ -68,49 +77,64 @@ exports.generate = async (req, res) => {
 	// Delete temporary files
 	fs.unlinkSync(jsonfile.path);
 	fs.unlinkSync(template_path);
+	fs.unlinkSync("Excel.xlsx");
 
 	return;
 };
 
-function readJSON_writeExcel(template_path, json) {
+async function readJSON_writeExcel(template_name, template_path, json) {
 	var workbook = new Excel.Workbook();
 
-	let success = workbook.xlsx
-		.readFile(template_path)
-		.then(function () {
-			let pages = Object.keys(json);
-			var firstLetterIdx = "A".charCodeAt() - 1;
+	await workbook.xlsx.readFile(template_path);
+	let pages = Object.keys(json);
+	var firstLetterIdx = "A".charCodeAt() - 1;
 
-			pages.forEach((element) => {
-				const pageJson = json[element];
-				const pagename = Object.keys(pageJson);
-				var worksheet = workbook.getWorksheet(String(pagename));
+	pages.forEach((element) => {
+		const pageJson = json[element];
+		const pagename = Object.keys(pageJson);
+		var worksheet = workbook.getWorksheet(String(pagename));
 
-				pagename.forEach((element) => {
-					const newJson = pageJson[element];
+		pagename.forEach((element) => {
+			const newJson = pageJson[element];
 
-					const fields = Object.keys(newJson);
-					fields.forEach((element) => {
-						var key = Object.keys(newJson[element]);
-						var col_row = String(key).match(/[a-zA-Z]+|[0-9]+/g);
-						var col_number =
-							col_row[0].charCodeAt() - firstLetterIdx;
-						worksheet.getRow(col_row[1]).getCell(col_number).value =
-							newJson[element][key];
-					});
-				});
+			const fields = Object.keys(newJson);
+			fields.forEach((element) => {
+				var key = Object.keys(newJson[element]);
+				var col_row = String(key).match(/[a-zA-Z]+|[0-9]+/g);
+				var col_number = col_row[0].charCodeAt() - firstLetterIdx;
+				worksheet.getRow(col_row[1]).getCell(col_number).value =
+					newJson[element][key];
 			});
-
-			workbook.xlsx.writeFile("Excel.xlsx");
-
-			// TODO: Store excel in s3 bucket
-
-			return true;
-		})
-		.catch((err) => {
-			console.error(err.message);
-			return false;
 		});
+	});
+	await workbook.xlsx.writeFile("Excel.xlsx");
 
-	return success;
+	// Store excel in s3 bucket
+	const fileContent = fs.readFileSync("Excel.xlsx");
+
+	const params = {
+		Bucket: process.env.AWS_BUCKET_NAME,
+		Key: "generated/" + template_name,
+		Body: fileContent,
+	};
+
+	await s3.upload(params, (err) => {
+		if (err) {
+			console.log("Erro no upload: ", err);
+		}
+		console.log("Sucesso no upload.");
+	});
+
+	const downloadURL = s3.getSignedUrl("getObject", {
+		Bucket: process.env.AWS_BUCKET_NAME,
+		Key: "generated/" + template_name,
+		Expires: 60 * 5,
+	});
+
+	/* .catch((err) => {
+		console.error(err.message);
+		return false;
+	}); */
+
+	return downloadURL;
 }
