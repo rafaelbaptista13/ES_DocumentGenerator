dfa3a2bd476589e49e217218c08f4e97c5358954
DiogoCarvalhoo <dfac@ua.pt>
2022-06-05 00:59:08 +0100

    start deploy with docker

--- web-app/scripts/after_install.sh
+++ web-app/scripts/after_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/web-app
 npm install 
+sudo docker build -t webapp .
+
+
--- web-app/scripts/app_start.sh
+++ web-app/scripts/app_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/web-app
 #npm start > app.out.log 2> app.err.log < /dev/null &
-serve -s build > app.out.log 2> app.err.log < /dev/null &
+#serve -s build > app.out.log 2> app.err.log < /dev/null &
+docker run -p 3000:3000 webapp
