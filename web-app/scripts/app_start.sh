39a87db7466efaa0d51784879ecfdf431962498a
DiogoCarvalhoo <dfac@ua.pt>
2022-06-05 01:19:09 +0100

    run container in detach mode

--- web-app/scripts/app_start.sh
+++ web-app/scripts/app_start.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/web-app
 #npm start > app.out.log 2> app.err.log < /dev/null &
 #serve -s build > app.out.log 2> app.err.log < /dev/null &
-docker run -p 3000:3000 webapp
+docker run -d -p 3000:3000 webapp
