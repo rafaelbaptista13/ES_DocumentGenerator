46f8019829689323c39356d80dca766c73129d09
dgcunhaua <dgcunha@ua.pt>
2022-06-06 23:50:04 +0100

    Swagger comment in document routes fixed, and url download fix

--- server/app/routes/document.routes.js
+++ server/app/routes/document.routes.js
@@ -20,6 +20,7 @@ module.exports = (app) => {
 	const document = require("../controllers/document.controller.js");
 	var router = require("express").Router();
 
+
 	/**
 	 * @swagger
 	 * /api/documents/:
@@ -27,14 +28,15 @@ module.exports = (app) => {
 	 *    description: Generate a document from a template and a JSON file
 	 *    responses:
 	 *      '200':
-	 *         description: Document generation was performed with success
-	 * 		'400':
-	 * 		   description: Bad request. Missing template name, missing json file or json file with wrong format.
-	 * 		'404':
-	 * 		   description: Requested template does not exist.
+	 *         description: Document generation was performed with success.
+	 *      '400':
+	 *         description: Bad request. Missing template name, missing json file or json file with wrong format.
+	 *      '404':
+	 *         description: Requested template does not exist.
 	 *      '500':
-	 *         description: An internal error has occoured
+	 *         description: An internal error has occoured.
 	 */
+	
 	router.post(
 		"/",
 		upload.fields([
--- web-app/src/Pages/Generate.js
+++ web-app/src/Pages/Generate.js
@@ -1,182 +1,236 @@
-import { React, useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap'; 
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
 
-import TemplateService from '../Services/template.service';
-import DocumentService from '../Services/document.service';
+import TemplateService from "../Services/template.service";
+import DocumentService from "../Services/document.service";
 
-function Generate (){
+function Generate() {
+	const [current_templates, setCurrentTemplates] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
+	const [successMessage, setSuccessMessage] = useState("");
+	const [selectedJSON, setSelectedJSON] = useState();
 
-    const [current_templates, setCurrentTemplates] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
-
-    const [selectedJSON, setSelectedJSON] = useState();
+	const [downloadURL, setDownloadURL] = useState();
 
 	const changeJSON = (event) => {
 		setSelectedJSON(event.target.files[0]);
 	};
 
+	function display_message(elementId) {
+		document.getElementById(elementId).style.display = "block";
+	}
 
-    function display_message(elementId) {
-        document.getElementById(elementId).style.display = "block";
-    }
+	function display_message(elementId) {
+		document.getElementById(elementId).style.display = "block";
+	}
+	function hide_message(id) {
+		var elemento = document.getElementById(id);
+		if (elemento !== undefined && elemento !== null)
+			elemento.style.display = "none";
+	}
 
-    function hide_message(id) {
-        var elemento = document.getElementById(id)
-        if (elemento !== undefined && elemento !== null)
-            elemento.style.display = "none"
-    }
+	function hide_everything() {
+		document.getElementById("sucessoGenerate").style.display = "none";
+		document.getElementById("erroRetrieve").style.display = "none";
+		document.getElementById("erroInvalidTemplate").style.display = "none";
+		document.getElementById("erroInvalidJson").style.display = "none";
+	}
 
-    function hide_everything() {
-        document.getElementById("sucessoGenerate").style.display = "none";
-        document.getElementById("erroRetrieve").style.display = "none";
-        document.getElementById("erroInvalidTemplate").style.display = "none";
-        document.getElementById("erroInvalidJson").style.display = "none";
+	async function generate_document(e) {
+		e.preventDefault();
+		hide_everything();
 
-    }
+		var template_element = document.getElementById("formTemplateSelect");
+		var template;
 
-    async function generate_document(e) {
-        e.preventDefault();
-        hide_everything();
+		if (template_element.selectedIndex !== 0) {
+			template =
+				template_element.options[template_element.selectedIndex].text;
+		} else {
+			display_message("erroInvalidTemplate");
+			return;
+		}
 
-        var template_element = document.getElementById("formTemplateSelect");
-        var template;
+		if (selectedJSON === undefined) {
+			display_message("erroInvalidJson");
+			return;
+		}
 
-        if (template_element.selectedIndex !== 0) {
-            template = template_element.options[template_element.selectedIndex].text;
-        } else {
-            display_message("erroInvalidTemplate")
-            return;
-        }
+		var res = await DocumentService.generate_document(
+			selectedJSON,
+			template
+		);
+		var json;
+		console.log(res);
 
-        if (selectedJSON === undefined) {
-            display_message("erroInvalidJson")
-            return;
-        }
+		if (res.status === undefined) {
+			setErrorMessage(res);
+			display_message("erroRetrieve");
+			return;
+		} else if (res.status !== 200) {
+			json = await res.json();
+			setErrorMessage(json["message"]);
+			display_message("erroRetrieve");
+			return;
+		}
 
-        var res = await DocumentService.generate_document(selectedJSON, template);
-        var json;
-        console.log(res);
+		json = await res.json();
+		setSuccessMessage(json["message"]);
+		display_message("sucessoGenerate");
+		setDownloadURL(json["downloadURL"]);
+	}
 
-        if (res.status === undefined) {
-            setErrorMessage(res)
-            display_message("erroRetrieve")
-            return
-        } 
-        else if (res.status !== 200) {
-            json = await res.json();
-            setErrorMessage(json["message"])
-            display_message("erroRetrieve")
-            return
-        }
+	useEffect(() => {
+		async function fetchCurrentTemplates() {
+			var res = await TemplateService.get_current_templates();
+			var json;
 
-        json = await res.json();
-        setSuccessMessage(json["message"])
-        display_message("sucessoGenerate")
+			if (res.status === undefined) {
+				setErrorMessage(res);
+				display_message("erroRetrieve");
+				return;
+			} else if (res.status !== 200) {
+				json = await res.json();
+				setErrorMessage(json["message"]);
+				display_message("erroRetrieve");
+				return;
+			}
 
-        var docWindow = window.open(json["downloadURL"], "_blank");
-        /*var form = document.getElementById("formAction")
-        var inputAWSAccessKeyID = document.getElementById("inputAWSAccessKeyID")
-        var inputExpires = document.getElementById("inputExpires")
-        var inputSignature = document.getElementById("inputSignature")
-        var button = document.getElementById("buttonToDownload");
+			json = await res.json();
 
-        var action = json["downloadURL"].split("?")[0]
-        form.action = action
+			setCurrentTemplates(json["templates"]);
+		}
 
-        var params = json["downloadURL"].split("?")[1]
+		fetchCurrentTemplates();
+	}, []);
+	return (
+		<div style={{ marginLeft: "10%", marginRight: "10%", marginTop: "2%" }}>
+			<h1>Document Generator</h1>
+			<br />
 
-        var params = params.split("&")
-        console.log(params)
-        var AWSAccessKeyId = params[0].substring(15);
-        var Expires = params[1].substring(8)
-        var Signature = params[2].substring(10)
-        console.log(Signature)
+			<div
+				id={"erroRetrieve"}
+				className="alert alert-danger row"
+				role="alert"
+				style={{
+					margin: "10px auto",
+					width: "90%",
+					textAlign: "center",
+					fontSize: "22px",
+					display: "none",
+					justifyContent: "center",
+				}}
+			>
+				{errorMessage}
+				<img
+					src={`${process.env.PUBLIC_URL}/crossicon.png`}
+					style={{ width: "5%", height: "auto", marginLeft: "8px" }}
+					alt={"Close Icon"}
+					onClick={() => hide_message("erroRetrieve")}
+				></img>
+			</div>
 
-        inputAWSAccessKeyID.value = AWSAccessKeyId;
-        inputExpires.value = Expires;
-        inputSignature.value = Signature;
-        button.click();*/
-    }
+			<div
+				id={"erroInvalidJson"}
+				className="alert alert-danger row"
+				role="alert"
+				style={{
+					margin: "10px auto",
+					width: "90%",
+					textAlign: "center",
+					fontSize: "22px",
+					display: "none",
+					justifyContent: "center",
+				}}
+			>
+				You must select a valid json file.
+				<img
+					src={`${process.env.PUBLIC_URL}/crossicon.png`}
+					style={{ width: "5%", height: "auto", marginLeft: "8px" }}
+					alt={"Close Icon"}
+					onClick={() => hide_message("erroInvalidJson")}
+				></img>
+			</div>
 
-    useEffect(() => {
-            async function fetchCurrentTemplates() {
+			<div
+				id={"erroInvalidTemplate"}
+				className="alert alert-danger row"
+				role="alert"
+				style={{
+					margin: "10px auto",
+					width: "90%",
+					textAlign: "center",
+					fontSize: "22px",
+					display: "none",
+					justifyContent: "center",
+				}}
+			>
+				You must select a valid template.
+				<img
+					src={`${process.env.PUBLIC_URL}/crossicon.png`}
+					style={{ width: "5%", height: "auto", marginLeft: "8px" }}
+					alt={"Close Icon"}
+					onClick={() => hide_message("erroInvalidTemplate")}
+				></img>
+			</div>
 
-                var res = await TemplateService.get_current_templates();
-                var json;
+			<div
+				id={"sucessoGenerate"}
+				className="alert alert-success row"
+				role="alert"
+				style={{
+					margin: "10px auto",
+					width: "90%",
+					textAlign: "center",
+					fontSize: "22px",
+					display: "none",
+					justifyContent: "center",
+				}}
+			>
+				{successMessage}
+				<img
+					src={`${process.env.PUBLIC_URL}/crossicon.png`}
+					style={{ width: "5%", height: "auto", marginLeft: "8px" }}
+					alt={"Close Icon"}
+					onClick={() => hide_message("sucessoGenerate")}
+				></img>
+			</div>
 
-                if (res.status === undefined) {
-                    setErrorMessage(res)
-                    display_message("erroRetrieve");
-                    return
-                }
-                else if (res.status !== 200) {
-                    json = await res.json();
-                    setErrorMessage(json["message"])
-                    display_message("erroRetrieve");
-                    return
-                }
+			<form onSubmit={generate_document}>
+				<span>Select a document template:</span>
+				<Form.Select
+					id="formTemplateSelect"
+					aria-label="Default select example"
+					defaultValue={"DEFAULT"}
+				>
+					<option value="DEFAULT" disabled hidden>
+						Choose a document template
+					</option>
+					{current_templates.map((template, key) => (
+						<option key={key} value={template}>
+							{template}
+						</option>
+					))}
+				</Form.Select>
+				<br />
+				<Form.Group controlId="formJsonFile" className="mb-3">
+					<Form.Label>Upload a JSON file</Form.Label>
+					<Form.Control type="file" onChange={changeJSON} />
+				</Form.Group>
+				<br />
+				<Button as="input" type="submit" value="Generate" />
+			</form>
 
-                json = await res.json()
-                
-                setCurrentTemplates(json["templates"]);
-            }
-
-            fetchCurrentTemplates()
-        }, []);
-    return ( 
-        <div style={{marginLeft: '10%', marginRight: '10%', marginTop: '2%'}}>
-            <h1>Document Generator</h1>
-            <br/>
-
-            <div id={"erroRetrieve"} className="alert alert-danger row" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px", display:"none", justifyContent: 'center'}} >
-                {errorMessage}
-                <img src={`${process.env.PUBLIC_URL}/crossicon.png`}   style={{width: "5%", height: "auto", marginLeft:"8px"}} alt={"Close Icon"} onClick={() => hide_message("erroRetrieve")}></img>
-            </div>
-
-            <div id={"erroInvalidJson"} className="alert alert-danger row" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px", display:"none", justifyContent: 'center'}} >
-                You must select a valid json file.
-                <img src={`${process.env.PUBLIC_URL}/crossicon.png`}   style={{width: "5%", height: "auto", marginLeft:"8px"}} alt={"Close Icon"} onClick={() => hide_message("erroInvalidJson")}></img>
-            </div>
-
-
-            <div id={"erroInvalidTemplate"} className="alert alert-danger row" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px", display:"none", justifyContent: 'center'}} >
-                You must select a valid template.
-                <img src={`${process.env.PUBLIC_URL}/crossicon.png`}   style={{width: "5%", height: "auto", marginLeft:"8px"}} alt={"Close Icon"} onClick={() => hide_message("erroInvalidTemplate")}></img>
-            </div>
-
-            <div id={"sucessoGenerate"} className="alert alert-success row" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px", display:"none", justifyContent: 'center'}}>
-                {successMessage}
-                <img src={`${process.env.PUBLIC_URL}/crossicon.png`}  style={{width: "5%", height: "auto", marginLeft:"8px"}} alt={"Close Icon"} onClick={() => hide_message("sucessoGenerate")}></img>
-            </div> 
-
-            <form onSubmit={generate_document}>
-                <span>Select a document template:</span>
-                <Form.Select id='formTemplateSelect' aria-label="Default select example" defaultValue={'DEFAULT'}>
-                    <option value="DEFAULT" disabled hidden>Choose a document template</option>
-                    {
-                        current_templates.map((template, key) => <option key={key} value={template}>{template}</option>)
-                    }
-                </Form.Select>
-                <br/>
-                <Form.Group controlId="formJsonFile" className="mb-3">
-                    <Form.Label>Upload a JSON file</Form.Label>
-                    <Form.Control type="file" onChange={changeJSON}/>
-                </Form.Group>
-                <br />
-                <Button as="input" type="submit" value="Generate" />
-            </form>
-
-
-            <form id="formAction" action="https://es-documents-templates-bucket.s3.amazonaws.com/documents/aa098700-e415-11ec-8aa0-a5194a3a2118.xlsx" method="get" target="_blank">
-                <input id="inputAWSAccessKeyID" type="hidden" name="AWSAccessKeyId" value="AKIAVKA25F67RAELW2MI" /> 
-                <input id="inputExpires" type="hidden" name="Expires" value="1654354524" /> 
-                <input id="inputSignature" type="hidden" name="Signature" value="fBxliSuse9dgY3qhyujKePBbpMg%3D" /> 
-
-                <button id="buttonToDownload" type="submit" style={{display: "none"}}></button>
-            </form>
-        </div>
-    )
+			{downloadURL !== undefined && (
+				<button type="button">
+					<a href={downloadURL} download>
+						Download File!
+					</a>
+				</button>
+			)}
+		</div>
+	);
 }
 
 export default Generate;
